1f099d812f6ae0b5ae556efd0f9968eebf9c6a13
joao.freitas <pjota.jpjoao28@gmail.com>
2023-09-04 21:53:53 -0300

    Primeiro commit

--- Projeto/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Projeto/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,13 +2,23 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Toast.makeText(this,
+            "Aula apocalíptica de Android",
+            Toast.LENGTH_LONG).show()
+        //Toast é o balão / makeT "qual texto tem
 
-        toast
+
+        val valor1 : EditText = findViewById(R.id.valor1)
+        val valor2 : EditText = findViewById(R.id.valor2)
+        valor1.text
+        valor2.text
     }
 }
